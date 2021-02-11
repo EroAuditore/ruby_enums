648e459dac0fcbbaf001d484d14b1a5733b9f867
EroAuditore <alfa.fenrir@gmail.com>
2021-02-11 13:31:07 -0600

    (new) added Each_with_index)

--- enumerable.rb
+++ enumerable.rb
@@ -4,13 +4,16 @@ module Enumerable
     
     def my_each
         return self.dup unless block_given?
-        length.times do |i|
-            yield to_a[i]
-          end
+        for i in 0.. self.length
+            yield self.to_a[i]
+        end
     end
 
     def my_each_with_index
         return self.dup unless block_given?
+        for i in 0.. self.length
+            yield  to_a[i] , i 
+        end
 
     end  
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
 require './enumerable.rb'
 
 [3, 2, 3, 4, 5].my_each { |num| puts num }
+
+[3, 2, 3, 4, 5].my_each_with_index { |num, index| puts "Num #{num} with index #{index}" }
