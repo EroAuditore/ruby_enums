b460881971a8bdf17f4fea95b592381cd48fb9a3
EroAuditore <alfa.fenrir@gmail.com>
2021-02-11 15:43:34 -0600

    (new) my_select method added

--- enumerable.rb
+++ enumerable.rb
@@ -12,10 +12,27 @@ module Enumerable
     def my_each_with_index
         return self.dup unless block_given?
         for i in 0.. self.length
-            yield  to_a[i] , i 
+            yield  self.to_a[i] , i 
         end
-
     end  
+
+    def my_each_with_index
+        return self.dup unless block_given?
+        for i in 0.. self.length
+            yield  self.to_a[i] , i 
+        end
+    end
+    
+    def my_select
+        new_arr = []
+        my_each do |item|
+            if yield(item)
+                new_arr << item
+            end 
+        end
+        new_arr
+    end
+    
 end
 
 
--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 require './enumerable.rb'
 
-[3, 2, 3, 4, 5].my_each { |num| puts num }
+#[3, 2, 3, 4, 5].my_each { |num| puts num }
 
-[3, 2, 3, 4, 5].my_each_with_index { |num, index| puts "Num #{num} with index #{index}" }
+#[3, 2, 3, 4, 5].my_each_with_index { |num, index| puts "Num #{num} with index #{index}" }
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_select {|friend| friend != 'Brian'}
+
+
+
+puts friends
