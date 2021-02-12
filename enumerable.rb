1fb40c1adbaecdc5f2ec9cd6d1b738f7d177ba7a
EroAuditore <alfa.fenrir@gmail.com>
2021-02-11 18:30:06 -0600

    (new my_all method added

--- enumerable.rb
+++ enumerable.rb
@@ -4,21 +4,22 @@ module Enumerable
     
     def my_each
         return self.dup unless block_given?
-        for i in 0.. self.length
+        for i in 0.. length - 1
             yield self.to_a[i]
         end
+        self
     end
 
     def my_each_with_index
         return self.dup unless block_given?
-        for i in 0.. self.length
+        for i in 0.. self.length - 1
             yield  self.to_a[i] , i 
         end
     end  
 
     def my_each_with_index
         return self.dup unless block_given?
-        for i in 0.. self.length
+        for i in 0.. self.length - 1
             yield  self.to_a[i] , i 
         end
     end
@@ -31,6 +32,17 @@ module Enumerable
             end 
         end
         new_arr
+    end
+
+    def my_all?
+        result = true
+        my_each do |item|
+            if !yield(item)
+                result = false
+            end
+            break
+        end
+        result
     end
     
 end
--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ require './enumerable.rb'
 
 #[3, 2, 3, 4, 5].my_each_with_index { |num, index| puts "Num #{num} with index #{index}" }
 
-friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_select {|friend| friend != 'Brian'}
+#result = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_select {|friend| friend != 'Brian'}
 
+result = %w[ant bear cat].my_all? { |word|  word.length >= 4 }
 
-
-puts friends
+puts result
