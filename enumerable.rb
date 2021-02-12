b6c9181a2f70af5302c5c8f8ada38a892239ced3
EroAuditore <alfa.fenrir@gmail.com>
2021-02-11 21:18:21 -0600

    (new) my_count added

--- enumerable.rb
+++ enumerable.rb
@@ -70,6 +70,17 @@ module Enumerable
         result
     end
     
+    def my_count(num=nil)
+        count = 0
+        if block_given?
+            my_each { |item|  count +=1 if yield(item)} 
+        elsif  !num.nil?
+            my_each { |item|  count +=1 if num==item} 
+        elsif num.nil? && !block_given?
+            my_each { |item|  count +=1 }
+        end
+        count
+    end
     
 end
 
--- main.rb
+++ main.rb
@@ -5,10 +5,15 @@ require './enumerable.rb'
 #[3, 2, 3, 4, 5].my_each_with_index { |num, index| puts "Num #{num} with index #{index}" }
 
 #result = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_select {|friend| friend != 'Brian'}
+
 #result = %w[ant bear cat].my_any?(/d/)  
 
-result = %w[ant bear cat].my_all?(/t/)                        #=> false
+#result = %w[ant bear cat].my_all?(/t/)                        #=> false
 
 #result = %w[ant bear cat].my_none?(/d/)                        #=> true
+
+ary = [1, 2, 4, 2, 4]
+
+result = ary.my_count 
 
 puts result
